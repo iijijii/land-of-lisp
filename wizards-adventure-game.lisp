934981f20af12fd9f41ca99f08fa71c5d6dd82d0
iijijii <lilysatoo@gmail.com>
2017-05-13 15:01:59 +0100

    Add *object-locations

--- wizards-adventure-game.lisp
+++ wizards-adventure-game.lisp
@@ -20,3 +20,8 @@
 	(apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
 
 (defparameter *objects* '(whiskey bucket frog chain))
+
+(defparameter *object-locations* '((whiskey living-room)
+																	 (bucket living-room)
+																	 (chain garden)
+																	 (frog garden)))
