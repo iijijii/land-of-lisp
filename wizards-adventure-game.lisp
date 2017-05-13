aba5e5b147f08a60b47e83abcb929ed55c183767
iijijii <lilysatoo@gmail.com>
2017-05-13 17:04:43 +0100

    Add game-read

--- wizards-adventure-game.lisp
+++ wizards-adventure-game.lisp
@@ -67,3 +67,10 @@
 		(unless (eq (car cmd) 'quit)
 			(game-print (game-eval cmd))
 			(game-repl))))
+
+(defun game-read()
+	(let ((cmd (read-from-string
+							 (concatenate 'string "(" (read-line) ")"))))
+		(flet ((quote-it (x)
+										 (list 'quote x)))
+			(cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
