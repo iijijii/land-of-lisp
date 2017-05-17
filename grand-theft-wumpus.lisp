50c26f87c852026890737e511d1781821a2773b4
iijijii <lilysatoo@gmail.com>
2017-05-17 13:54:09 +0100

    Enable to make edge list

--- grand-theft-wumpus.lisp
+++ grand-theft-wumpus.lisp
@@ -7,3 +7,14 @@
 (defparameter *edge-num* 45)
 (defparameter *worm-num* 3)
 (defparameter *cop-odds* 15)
+
+(defun random-node ()
+	(1+ (random *node-num*)))
+
+(defun edge-pair (a b)
+	(unless (eql a b)
+		(list (cons a b) (cons b a))))
+
+(defun make-edge-list ()
+	(apply #'append (loop repeat *edge-num*
+												collect (edge-pair (random-node) (random-node)))))
